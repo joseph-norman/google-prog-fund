4f1cffafc1b6dcf173f87fdd80c7e308ff0ac7ad
Joe Norman <josephwlnorman@gmail.com>
2020-02-02 14:38:15 +0000

    Tidy it up a little bit

--- 01_subsequence.py
+++ 01_subsequence.py
@@ -27,39 +27,28 @@
 # This question gives you the chance to practice with algorithms and data structures.
 # It's also a good example of why careful analysis for Big-O performance is often
 # worthwhile, as is careful exploration of common and worst-case input conditions.
-# 
 
-# From S: iteratively remove non-matching characters and do an equality check at each step.
-# Retain a swap value for current and previously matching word until we're done going through W.
-
-def subsequence(words):
-    subsequence = "abpppleee"
+def subsequence(words, subsequence):
+    # Using a set() data structure so that we only retain unique values (removes duplicates)
     guesses = set()
 
-    # For each word
-    for ind, word in enumerate(words):
+    for word in words:
         guess = ""
 
         # Check we can actually compare before iterating through subseq.
         for i in word:
             if i not in subsequence:
-                print("No point even checking this one (should be kangaroo)", word)
+                break
 
         # for each char in subsequence
         for char in subsequence:
-
             # If it does not exist in word
             if char not in word:
-                # Remove it in its current pos.
+                # Remove it in its current position. Important to retain ordering for subsequence!
                 guess = subsequence.replace(char, "")
 
             for j in guess:
-                # Count the chars
-                # If the count of j in guess is > count of j in word
-                # Remove the number of chars based on diff
                 charDiff = guess.count(j) - word.count(j)
-                # print(j, charDiff, word)
-
                 # Remove occurrences that are extraneous.
                 guess = guess.replace(j, "", charDiff)
 
@@ -67,19 +56,17 @@ def subsequence(words):
                 # Store count of words.
                 guesses.add(guess)
     
+    # Converting back to a list for the comparison of each value based on simple loop+cache
     enumerable = list(guesses)
     longest = ""
-    # Finally, get the longest
     for idx, val in enumerate(list(guesses)):
-       print(idx, val)
        if len(val) > len(enumerable[idx-1]):
            longest = val
     
-    print("Longest subsequence possible in", subsequence, " is", longest)
-
+    print("Longest subsequence possible in " + subsequence + " is " + longest)
 
 def main():
-    subsequence(["able", "ale", "apple", "bale", "kangaroo"])
+    subsequence(["able", "ale", "apple", "bale", "kangaroo"], "abpppleee")
 
 if __name__ == "__main__":
     main()
